a79af1dc5e96577a694beefa4c0e3a9580134369
denisk <deniskomsikov@gmail.com>
2024-10-28 02:04:08 +0300

    edit rus models

---
+++ packages/llm-info/src/providers/yandex.ts
@@ -0,0 +1,20 @@
+import { ModelProvider } from "../types.js";
+
+export const Yandex: ModelProvider = {
+  id: "yandexgpt",
+  displayName: "YandexGPT",
+  models: [
+    {
+      model: "yandexgpt",
+      displayName: "YandexGPT",
+      contextLength: 16_385,
+      maxCompletionTokens: 4096,
+    },
+    {
+      model: "yandexgpt",
+      displayName: "YandexGPT-Lite",
+      contextLength: 16_385,
+      maxCompletionTokens: 4096,
+    },
+  ],
+}
